ab007a1953a26fe9eed54ccb42ed13e95cd2b4f2
Ðîîñòèñëà Óñïàñêèé <ruspasskiy2002@gmail.com>
2023-01-13 11:07:42 +0300

    Бот оформлен по PEP8

--- handlers/other_handlers.py
+++ handlers/other_handlers.py
@@ -8,6 +8,5 @@ async def send_answer(message: Message):
     await message.answer(text=LEXICON_RU['other_answer'])
 
 
-
 def register_other_handlers(dp: Dispatcher):
     dp.register_message_handler(send_answer)
